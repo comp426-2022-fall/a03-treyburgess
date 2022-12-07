ce334d0dc2afd44fe4418e0feff885a99779e210
Trey Burgess <treyb@UNC-PF2AAVE3.localdomain>
2022-12-07 05:14:29 -0500

    done already?

--- bin/cli.js
+++ bin/cli.js
@@ -1,1 +1,12 @@
 #!/usr/bin/env node
+
+import { roll } from '../lib/roll.js'
+import minimist from 'minimist'
+
+const args = minimist(process.argv.slice(2));
+
+const sides = args.sides || 6;
+const dice = args.dice || 2;
+const rolls = args.rolls || 1;
+
+console.log(roll(sides, dice, rolls));
